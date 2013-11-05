75fee90788a5c33d93c529b475181b4e9e59e2de
Michael Rose <elementation@gmail.com>
2013-11-05 13:48:32 -0700

    0.6.0: Deprecating timeoutSeconds parameter

--- lib/fullcontact/client/person.rb
+++ lib/fullcontact/client/person.rb
@@ -8,6 +8,12 @@ module FullContact
            warn "[DEPRECATION] supplying an email address directly is deprecated.  Please use {email: #{options}} instead."
            options = {:email => options}
          end
+
+         if options.has_key?(:timeoutSeconds)
+           options.delete(:timeoutSeconds)
+           warn '[DEPRECATION] timeoutSeconds is no longer a supported parameter. It has been removed from your request.'
+         end
+
         response = get('person', options, false, faraday_options)
         format.to_s.downcase == 'xml' ? response['person'] : response
       end
